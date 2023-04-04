09ba0e48d00e05d97abc8d8b056ab1671d5d4299
BoumouzounaBrahimVall <brahimvall.boumouzouna@etu.fstm.ac.ma>
2023-04-04 20:32:59 +0000

    added ids and used f format

--- algo_impl/data_mining/NaiveBayes.py
+++ algo_impl/data_mining/NaiveBayes.py
@@ -2,9 +2,9 @@ from typing import List, Tuple
 from collections import defaultdict
 
 
-def naive_bayes(training_data: List[List[str]], feature_names: List[str], classes: List[str],
+def naive_bayes(training_data: List[List[str]], ids: List[str], classes: List[str],
                 test_data: List[str]) -> str:
-    nbr_features = len(feature_names)
+    nbr_features = len(ids)
     nbr_classes = len(classes)
 
     classe_counts = defaultdict(int)
@@ -17,7 +17,7 @@ def naive_bayes(training_data: List[List[str]], feature_names: List[str], classe
         # pour chaque ligne
         for i in range(nbr_features):
             feature = data[i]  # recuperer valuer propriete de la ligne
-            feature_classe_key = feature_names[i] + "_" + classe  # stocker nomPropriete_valeurClasse ex: Temps_Oui
+            feature_classe_key = ids[i] + "_" + classe  # stocker nomPropriete_valeurClasse ex: Temps_Oui
             feature_counts[feature_classe_key][feature] += 1  # ajouter <"Soleil" , 2 + 1>
 
     # Calculer les probabilités pour chaque classe
@@ -25,20 +25,19 @@ def naive_bayes(training_data: List[List[str]], feature_names: List[str], classe
     for i in range(nbr_classes):
         classe = classes[i]
 
-        print("-------------------------Pour la Classe: ({})-------------------------".format(classe))
+        print(f"-------------------------Pour la Classe: ({classe})-------------------------")
         probability = classe_counts[classe] / len(training_data)  # probabilite de la classe
-        print("P({})= {}/{} = {}".format(classe, classe_counts[classe], len(training_data), probability))
+        print(f"P({classe})= {classe_counts[classe]}/{len(training_data)} = {probability}")
         for j in range(nbr_features):  # pour chaque valeur propreite
             feature = test_data[j]
-            feature_label_key = feature_names[j] + "_" + classe
+            feature_label_key = ids[j] + "_" + classe
             feature_classe_counts = feature_counts[feature_label_key]
             feature_count = feature_classe_counts.get(feature, 0)  # recuperer ls
             feature_classe_prob = feature_count / classe_counts[classe]
-            print("propriete: {}, valeur: ({}), nombre pour la classe:({}): {}\n\t P({}|{}) = {}/{} ={}".format(
-                feature_names[j], feature, classe, feature_count, feature, classe, feature_count, classe_counts[classe], feature_classe_prob))
+            print(f"propriete: {ids[j]}, valeur: ({feature}), nombre pour la classe:({classe}): {feature_count}\n\t P({feature}|{classe}) = {feature_count}/{classe_counts[classe]} ={feature_classe_prob}")
             probability *= feature_classe_prob
 
-        print("\t\t==========Probabilite de la classe ({}): {} ==========".format(classe, probability))
+        print(f"\t\t==========Probabilite de la classe ({classe}): {probability} ==========")
         print()
         classes_probabilities[i] = probability
 
@@ -64,10 +63,10 @@ training_data = [
     ["1", "0", "1", "0", "1", "C2"]
 ]
 
-feature_names = ["x1", "x2", "x3", "x4", "x5"]
+ids = ["x1", "x2", "x3", "x4", "x5"]
 classes = ["C1", "C2"]
 
 test_data = ["0", "0", "1", "1", "1"]
 
-result = naive_bayes(training_data, feature_names, classes, test_data)
+result = naive_bayes(training_data, ids, classes, test_data)
 print(f"La prédiction pour les données de test {test_data} est: {result}")
